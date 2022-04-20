7999213268f1b22720a8d02f1192a57981a04bc7
skhreis <skhreis0@gmail.com>
2022-04-20 12:44:32 -0400

    Created Car Rental Project.

---
+++ car_rentals.js
@@ -0,0 +1,53 @@
+const carRentals = [
+	[	
+		{Company: 'Enterprise'},
+		{carType: 'Sedan'},
+		{doorCount: 4},
+		{carBrand: 'Toyota'},
+		{carModel: 'Camry'},
+		{carYear: 2020},
+		{carImg: 'https://cdn.jdpower.com/ChromeImageGallery/Expanded/Transparent/640/2020TOC02_640/2020TOC020027_640_07.png'},
+	],
+	
+	[	
+		{Company: 'Budget'},
+		{carType: 'SUV'},
+		{doorCount: 4},
+		{carBrand: 'GMC'},
+		{carModel: 'Yukon'},
+		{carYear: 2019},
+		{carImg: 'https://cars.usnews.com/static/images/Auto/izmo/i94428615/2019_gmc_yukon_xl_angularfront.jpg'}
+	],
+
+	[	
+		{Company: 'Enterprise'},
+		{carType: 'Coupe'},
+		{doorCount: 2},
+		{carBrand: 'BMW'},
+		{carModel: 'M4'},
+		{carYear: 2022},
+		{carImg: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bmw-m4-x-kith-production-1-of-150-frozen-black-1-1603393948.jpg?crop=0.814xw:0.544xh;0.130xw,0.308xh&resize=1200:*'}
+	],
+
+	[	
+		{Company: 'Alamo'},
+		{carType: 'Sedan'},
+		{doorCount: 4},
+		{carBrand: 'Honda'},
+		{carModel: 'Accord'},
+		{carYear: 2020},
+		{carImg: 'https://images.dealer.com/ddc/vehicles/2022/Honda/Accord/Sedan/color/Modern%20Steel%20Metallic-GY-64,62,63-640-en_US.jpg'}
+	],
+
+	[	
+		{Company: 'Alamo'},
+		{carType: 'Convertible'},
+		{doorCount: 2},
+		{carBrand: 'Chevrolet'},
+		{carModel: 'Camaro'},
+		{carYear: 2022},
+		{carImg: 'https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2022/performance/camaro/02-images/mov/2022-camaro-masthead-01.jpg?imwidth=600'}
+	]
+]
+
+console.log(carRentals)
