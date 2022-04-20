88b6fad6de1276c61774d806a33fd2c8407297eb
skhreis <skhreis0@gmail.com>
2022-04-20 13:52:20 -0400

    Server up and running.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+
+const PORT = 6000
+
+app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
+
+const carRentals = require('./models/car_rentals.js')
+
+app.get('/', (req,res) => {
+	res.send('<h1>Hey</h1>')
+})
+
+
+app.listen(PORT, console.log(`Listening on ${PORT}`))
